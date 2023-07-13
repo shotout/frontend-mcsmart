65d4a8bda7d5d1e95354395a683bac69f3100fd4
Sendipat04 <sendi.patryjar@rey.id>
2023-07-13 16:34:26 +0700

    add tracking

--- src/App.js
+++ src/App.js
@@ -44,9 +44,7 @@ const App =  () => {
   };
   Settings.initializeSDK();
   Settings.setAppID('637815961525510');
-
-
- 
+  
   useEffect(async() => {
     Notifications.removeAllDeliveredNotifications();
     networkDebugger();
