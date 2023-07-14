1ae87b65ef4c22d94b3df12af4127dc722ce3d8c
Sendipat04 <sendi.patryjar@rey.id>
2023-07-14 11:00:02 +0700

    merge

--- ios/mcsmart/AppDelegate.mm
+++ ios/mcsmart/AppDelegate.mm
@@ -23,7 +23,20 @@
                        didFinishLaunchingWithOptions:launchOptions];
   return didFinish;
 }
+- (BOOL)application:(UIApplication *)app
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  if ([[FBSDKApplicationDelegate sharedInstance] application:app openURL:url options:options]) {
+    return YES;
+  }
 
+  if ([RCTLinkingManager application:app openURL:url options:options]) {
+    return YES;
+  }
+
+  return NO;
+}
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
