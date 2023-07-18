8224642d385861f721636b1c1c14ab82becc8cd3
I <->
2023-07-18 09:03:44 +0700

    select category

--- src/layout/setting/change-icon/index.js
+++ src/layout/setting/change-icon/index.js
@@ -1,5 +1,5 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, {useEffect, useRef, useState} from 'react';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Alert,
   Image,
@@ -8,37 +8,40 @@ import {
   Text,
   TouchableWithoutFeedback,
   View,
-} from 'react-native';
-import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
-import {changeIcon, getIcon} from 'react-native-change-icon';
-import {RewardedAd, RewardedAdEventType} from 'react-native-google-mobile-ads';
-import HeaderButton from '../../../components/header-button';
-import styles from './styles';
-import dispatcher from './dispatcher';
-import states from './states';
+} from "react-native";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { changeIcon, getIcon } from "react-native-change-icon";
+import {
+  RewardedAd,
+  RewardedAdEventType,
+} from "react-native-google-mobile-ads";
+import HeaderButton from "../../../components/header-button";
+import styles from "./styles";
+import dispatcher from "./dispatcher";
+import states from "./states";
 import {
   iconNameToId,
   isUserPremium,
   reloadUserProfile,
-} from '../../../helpers/user';
+} from "../../../helpers/user";
 
-import IconLockWhite from '../../../assets/svg/lock_white_icon.svg';
-import ModalUnlockCategory from '../../../components/modal-unlock-ads';
-import {getRewardedCategoryID} from '../../../shared/static/adsId';
-import {updateProfile} from '../../../shared/request';
-import LoadingIndicator from '../../../components/loading-indicator';
+import IconLockWhite from "../../../assets/svg/lock_white_icon.svg";
+import ModalUnlockCategory from "../../../components/modal-unlock-ads";
+import { getRewardedCategoryID } from "../../../shared/static/adsId";
+import { updateProfile } from "../../../shared/request";
+import LoadingIndicator from "../../../components/loading-indicator";
 
-const bannerIcon1 = require('../../../assets/app_icon_asset/first.png');
-const bannerIcon2 = require('../../../assets/app_icon_asset/second_new.png');
-const bannerIcon3 = require('../../../assets/app_icon_asset/third.png');
-const bannerIcon4 = require('../../../assets/app_icon_asset/fourth.png');
+const bannerIcon1 = require("../../../assets/app_icon_asset/first.png");
+const bannerIcon2 = require("../../../assets/app_icon_asset/second_new.png");
+const bannerIcon3 = require("../../../assets/app_icon_asset/third.png");
+const bannerIcon4 = require("../../../assets/app_icon_asset/fourth.png");
 
-const adsIcon = require('../../../assets/icons/ads_icon.png');
+const adsIcon = require("../../../assets/icons/ads_icon.png");
 
 const rewarded = RewardedAd.createForAdRequest(getRewardedCategoryID(), {
   requestNonPersonalizedAdsOnly: true,
-  keywords: ['fashion', 'clothing'],
+  keywords: ["fashion", "clothing"],
 });
 
 function ModalChangeIcon({
@@ -55,32 +58,32 @@ function ModalChangeIcon({
   const listIcon = [
     {
       id: 1,
-      name: 'first',
+      name: "first",
       icon: bannerIcon1,
     },
     {
       id: 2,
-      name: 'second',
+      name: "second",
       icon: bannerIcon2,
     },
     {
       id: 3,
-      name: 'third',
+      name: "third",
       icon: bannerIcon3,
     },
     {
       id: 4,
-      name: 'fourth',
+      name: "fourth",
       icon: bannerIcon4,
     },
   ];
 
   useEffect(() => {
     const getInitialIcon = async () => {
-      const isSetBefore = await AsyncStorage.getItem('customIcon');
+      const isSetBefore = await AsyncStorage.getItem("customIcon");
 
       const resIcon = await getIcon();
-      console.log('Check resIcon', resIcon);
+      console.log("Check resIcon", resIcon);
       if (isSetBefore) {
         setSelectedIcon(iconNameToId(isSetBefore));
       }
@@ -89,16 +92,16 @@ function ModalChangeIcon({
     const unsubscribeLoaded = rewarded.addAdEventListener(
       RewardedAdEventType.LOADED,
       () => {
-        console.log('LOAD ADS change icon');
-      },
+        console.log("LOAD ADS change icon");
+      }
     );
     const unsubscribeEarned = rewarded.addAdEventListener(
       RewardedAdEventType.EARNED_REWARD,
-      reward => {
+      (reward) => {
         if (reward) {
           handleChangeIcon(selectedTemporary.current);
         }
-      },
+      }
     );
 
     return () => {
@@ -111,33 +114,32 @@ function ModalChangeIcon({
     rewarded.load();
   }, [isVisible]);
 
-  const handleSubmit = async item => {
+  const handleSubmit = async (item) => {
     try {
       const payload = {
         icon: item,
-        _method: 'PATCH',
+        _method: "PATCH",
       };
       await updateProfile(payload);
       reloadUserProfile();
     } catch (err) {
-      console.log('Error select:', err);
+      console.log("Error select:", err);
     }
   };
 
-  const handleChangeIcon = icon => {
-    if (icon.id !== selectedIcon) {
-      changeIcon(icon.name)
-        .then(async () => {
-          setSelectedIcon(icon.id);
-          await AsyncStorage.setItem('customIcon', icon.name);
-          handleSubmit(icon.id);
-          // selectModal();
-        })
-        .catch(e => {
-          Alert.alert("Sorry, can't change icon at this time.");
-          console.log('Error change icon:', e.message);
-        });
-    }
+  const handleChangeIcon = (icon) => {
+    changeIcon(icon.name)
+      .then(async () => {
+        setSelectedIcon(icon.id);
+        await AsyncStorage.setItem("customIcon", icon.name);
+        handleSubmit(icon.id);
+        // selectModal();
+      })
+      .catch((e) => {
+        Alert.alert("Sorry, can't change icon at this time.");
+        console.log("Error change icon:", e.message);
+      });
+
   };
 
   function renderInput() {
@@ -154,33 +156,40 @@ function ModalChangeIcon({
               onPress={async () => {
                 if (!isUserPremium() && icon.id !== 1) {
                   selectedTemporary.current = icon;
-                  if (rewarded.loaded) {
-                    rewarded.show();
+                  if (icon.id === selectedIcon) {
+                    handleChangeIcon(icon);
                   } else {
-                    setLoadingAds(true);
-                    rewarded.load();
-                    setTimeout(() => {
-                      if (rewarded.loaded) {
-                        rewarded.show();
-                        setLoadingAds(false);
-                      } else {
-                        setLoadingAds(false);
-                      }
-                    }, 3000);
+                    if (rewarded.loaded) {
+                      rewarded.show();
+                    } else {
+                      setLoadingAds(true);
+                      rewarded.load();
+                      setTimeout(() => {
+                        if (rewarded.loaded) {
+                          rewarded.show();
+                          setLoadingAds(false);
+                        } else {
+                          setLoadingAds(false);
+                        }
+                      }, 3000);
+                    }
                   }
                 } else {
                   handleChangeIcon(icon);
                 }
-              }}>
+              }}
+            >
               <View
                 style={[
                   styles.iconWrapper,
                   selectedIcon === icon.id && styles.activeIcon,
-                ]}>
+                ]}
+              >
                 <View style={[styles.ctnIconApp]}>
                   <ImageBackground
                     source={icon.icon}
-                    style={styles.ctnIconStyle}>
+                    style={styles.ctnIconStyle}
+                  >
                     {!isUserPremium() && icon.id !== selectedIcon && (
                       <View style={styles.ctnSelectPremium}>
                         <View style={styles.ctnIconIndicator}>
@@ -214,7 +223,8 @@ function ModalChangeIcon({
       visible={isVisible}
       animationType="fade"
       transparent
-      onDismiss={onClose}>
+      onDismiss={onClose}
+    >
       <View style={styles.ctnRoot}>
         <View style={styles.ctnContent}>
           <HeaderButton title="Change app icon" onPress={onClose} />
